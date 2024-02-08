c54046049daf8af951e8f5ee4da94e6e42ece828
Basheera <01fe22bca203@Kletech.ac.in>
2024-02-08 10:42:13 +0530

    Code realted to basic arith ops

---
+++ Calculator.java
@@ -0,0 +1,38 @@
+class Calculator{
+    float addition(float op1, float op2){
+        return(op1+op2);
+    }
+    float subtraction(float op1, float op2){
+        return(op1-op2);
+    }
+    float multiplication(float op1, float op2){
+        return(op1*op2);
+    }
+    /**
+     * The division function takes two float operands and returns their sum.
+     * 
+     * @param op1 The first operand for the division operation.
+     * @param op2 The op2 parameter is a float type variable that represents the second operand in the
+     * division operation.
+     * @return The sum of op1 and op2 is being returned.
+     */
+    float division(float op1, float op2){
+        return(op1/op2);
+    }
+
+        public static void main(String args[]){
+            float op1=10;
+            float op2=5;
+            Calculator calc =new Calculator();
+            float add_result=calc.addition(op1,op2);
+            System.out.println("Addition: "+add_result);
+            float sub_result=calc.subtraction(op1,op2);
+            System.out.println("Subtraction: "+sub_result);
+            float mul_result=calc.multiplication(op1,op2);
+            System.out.println("Multiplication: "+mul_result);
+            float div_result=calc.division(op1,op2);
+            System.out.println("Division: "+div_result);
+            
+
+        }   
+    }
